8b9500b2f7930e5a220ed38b821995d7c6c43f80
dknoma <djknoma@gmail.com>
2019-11-06 11:15:09 -0800

    SUBSCRIBE invalid fixed header flags test

--- src/test/java/org/reaktivity/specification/mqtt/streams/ConnectionIT.java
+++ src/test/java/org/reaktivity/specification/mqtt/streams/ConnectionIT.java
@@ -86,6 +86,18 @@ public class ConnectionIT
 
     @Test
     @Specification({
+        "${scripts}/subscribe/invalid.fixed.header.flags/client",
+        "${scripts}/subscribe/invalid.fixed.header.flags/server"})
+    @ScriptProperty("serverTransport \"nukleus://streams/mqtt#0\"")
+    public void shouldRejectMalformedSubscribePacket() throws Exception
+    {
+        k3po.start();
+        k3po.notifyBarrier("ROUTED_SERVER");
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({
         "${scripts}/unsubscribe/invalid.fixed.header.flags/client",
         "${scripts}/unsubscribe/invalid.fixed.header.flags/server"})
     @ScriptProperty("serverTransport \"nukleus://streams/mqtt#0\"")
