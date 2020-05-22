e0d93978ed9a222708e71f044da1ad1ca0be9f86
dknoma <djknoma@gmail.com>
2020-05-21 22:02:54 -0700

    added tests for rejecting multiple topicAliasMaximum in CONNECT and topicAlias in PUBLISH

--- src/test/java/org/reaktivity/specification/nukleus/mqtt/streams/StreamIT.java
+++ src/test/java/org/reaktivity/specification/nukleus/mqtt/streams/StreamIT.java
@@ -279,8 +279,8 @@ public class StreamIT
 
     @Test
     @Specification({
-        "${scripts}/reject.publish.with.topic.alias.more.than.maximum/client",
-        "${scripts}/reject.publish.with.topic.alias.more.than.maximum/server"})
+        "${scripts}/publish.fails/client",
+        "${scripts}/publish.fails/server"})
     @ScriptProperty("serverTransport \"nukleus://streams/mqtt#0\"")
     public void shouldRejectPublishWithTopicAliasMoreThanMaximum() throws Exception
     {
