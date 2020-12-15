288a76c3e740ee61a597f8ec41f993d39795a320
dknoma <djknoma@gmail.com>
2020-12-15 13:50:56 -0800

    scenario ensure suback comes before publish

--- src/test/java/org/reaktivity/specification/nukleus/mqtt/streams/StreamIT.java
+++ src/test/java/org/reaktivity/specification/nukleus/mqtt/streams/StreamIT.java
@@ -516,40 +516,4 @@ public class StreamIT
         k3po.notifyBarrier("ROUTED_CLIENT");
         k3po.finish();
     }
-
-    @Test
-    @Specification({
-        "${scripts}/connect.clean.start.when.session.present.with.will.message/client",
-        "${scripts}/connect.clean.start.when.session.present.with.will.message/server"})
-    @ScriptProperty("serverTransport \"nukleus://streams/mqtt#0\"")
-    public void shouldConnectWithCleanStart1AndWillMessage() throws Exception
-    {
-        k3po.start();
-        k3po.notifyBarrier("ROUTED_CLIENT");
-        k3po.finish();
-    }
-
-    @Test
-    @Specification({
-        "${scripts}/connect.when.session.present.with.will.message/client",
-        "${scripts}/connect.when.session.present.with.will.message/server"})
-    @ScriptProperty("serverTransport \"nukleus://streams/mqtt#0\"")
-    public void shouldConnectWithCleanStart0AndWillMessage() throws Exception
-    {
-        k3po.start();
-        k3po.notifyBarrier("ROUTED_CLIENT");
-        k3po.finish();
-    }
-
-    @Test
-    @Specification({
-        "${scripts}/subscribe.one.message.suback.order/client",
-        "${scripts}/subscribe.one.message.suback.order/server"})
-    @ScriptProperty("serverTransport \"nukleus://streams/mqtt#0\"")
-    public void shouldSubscribeOneMessage() throws Exception
-    {
-        k3po.start();
-        k3po.notifyBarrier("ROUTED_CLIENT");
-        k3po.finish();
-    }
 }
